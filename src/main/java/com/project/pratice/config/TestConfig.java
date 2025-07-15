035987124f91050a7418eaa23837c0648072a1e8
kaua-pugliesi <kaua.pugliesi78@gmail.com>
2025-07-14 22:13:40 -0300

    Payment, one-to-one association

--- src/main/java/com/project/pratice/config/TestConfig.java
+++ src/main/java/com/project/pratice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.project.pratice.entities.Category;
 import com.project.pratice.entities.Order;
 import com.project.pratice.entities.OrderItem;
+import com.project.pratice.entities.Payment;
 import com.project.pratice.entities.Product;
 import com.project.pratice.entities.User;
 import com.project.pratice.entities.enums.OrderStatus;
@@ -83,6 +84,10 @@ public class TestConfig implements CommandLineRunner {
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,  oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPaymnet(pay1);
+		
+		orderRepository.save(o1);
 		
 	}
 	
