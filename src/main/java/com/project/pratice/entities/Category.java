014ac1e0a2b8923dd41a25dac4680a4cf1aabca0
kauapugliesi <pugliesikaua@gmail.com>
2025-07-16 12:06:24 -0300

    Insert, Delete and Update user

--- src/main/java/com/project/pratice/entities/Category.java
+++ src/main/java/com/project/pratice/entities/Category.java
@@ -11,7 +11,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Table;
 
 
--- src/main/java/com/project/pratice/services/UserService.java
+++ src/main/java/com/project/pratice/services/UserService.java
@@ -3,6 +3,7 @@ package com.project.pratice.services;
 import java.util.List;
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +23,26 @@ public class UserService {
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
+	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity, obj);
+		return userRepository.save(entity);
+				
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
